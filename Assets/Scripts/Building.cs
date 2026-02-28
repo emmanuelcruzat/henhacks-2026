e670a7daa2bbf3ea65050a803bb560113a88dbd7
Luke Bonniwell <113559516+Lukebonn@users.noreply.github.com>
2026-02-28 15:14:33 -0500

    v2 of building thingies

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,16 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Building : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public int cost;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,8 +10,27 @@ public class GameManager : MonoBehaviour
     public int gold;
     public TextMeshProUGUI goldDisplay;
 
+    private Building buildingToPlace;
+    public GameObject grid;
+
+    public CustomCursor customCursor;
+
     private void Update()
     {
         goldDisplay.text = gold.ToString();
+    }
+
+    public void BuyBuilding(Building building)
+    {
+        if (gold >= building.cost)
+        {
+            customCursor.gameObject.SetActive(true);
+            customCursor.GetComponent<SpriteRenderer>().sprite = building.GetComponent<SpriteRenderer>().sprite;
+            Cursor.visible = false;
+
+            gold -= building.cost;
+            buildingToPlace = building;
+            grid.SetActive(true);
+        }
     }
 }
